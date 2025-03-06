1eec8d5c225bc419630b2b59b23789c92c9a78c8
lambsouls <70055403+lambsouls@users.noreply.github.com>
2025-03-06 20:15:09 +0800

    Delete mods/50_stream_demo.py

--- mods/50_stream_demo.py
+++
@@ -1,9 +0,0 @@
-async def preprocess(data: dict, request):
-    """预处理"""
-    print("\n预处理...")
-    return data
-
-async def postprocess(data: dict, request):
-    """后处理"""
-    print(f"\n后处理...")
-    return data
